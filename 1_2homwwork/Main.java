b12dbf1062432bdc298e0f8de2483837b06d0d30
VAN1119 <bnaoya@yahoo.co.jp>
2021-12-05 15:41:51 +0900

    new file

---
+++ 1_2homwwork/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+	public static void main(String[] args){
+		Shop s1 = new Shop("A’¬“X", 150000);
+		Shop s2 = new Shop("BŒö‰€‘O“X", 180000);
+		Shop s3 = new Shop();
+		
+		s1.display();
+		s2.display();
+		s3.display();
+	}
+}
---
+++ 1_2homwwork/Shop.java
@@ -0,0 +1,17 @@
+public class Shop {
+	String name;
+	int sales;
+	
+	public Shop(String name, int sales) {
+		this.name = name;
+		this.sales = sales;
+	}
+	
+	public Shop() {
+		this("o“X—\’è", 0);
+	}
+	
+	public void display() {
+		System.out.println(this.name + ":”„ã‚" +this.sales + "‰~");
+	}
+}
