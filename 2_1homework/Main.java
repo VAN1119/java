4bcd364c203695b96901776a5658158084a69d19
VAN1119 <bnaoya@yahoo.co.jp>
2021-12-07 09:51:52 +0900

    new file

---
+++ 2_1homework/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		Item ringo = new Item();
+		Item banana = new Item();
+		ringo.name = "ƒŠƒ“ƒS";
+		ringo.price = 150;
+		banana.name = "ƒoƒiƒi";
+		banana.price = 100;
+		System.out.println(ringo.name);
+		System.out.println(ringo.price);
+		System.out.println(banana.name);
+		System.out.println(banana.price);
+	}
+}
