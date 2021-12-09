2d2800840f5471f690f9d4f36dcd15af562a0f18
VAN1119 <bnaoya@yahoo.co.jp>
2021-12-09 10:41:03 +0900

    new file

---
+++ 2_2homework/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		Shop s1 = new Shop();
+		Shop s2 = new Shop();
+		s1.name = "ƒpƒ“‰®";
+		s1.sale = 100000;
+		s2.name = "—m•‰®";
+		s2.sale = 150000;
+		System.out.println(s1.name + " ”„ã:" + s1.sale);
+		System.out.println(s2.name + " ”„ã:" + s2.sale);
+	}
+}
